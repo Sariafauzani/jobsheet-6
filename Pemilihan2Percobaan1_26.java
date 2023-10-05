5973d1d8d6c6407484038dfcbb480f80288edce6
Sariafauzani <ajas2654@gmail.com>
2023-10-05 12:09:59 +0700

    modif 1

--- Pemilihan2Percobaan1_26.java
+++ Pemilihan2Percobaan1_26.java
@@ -10,7 +10,7 @@ public class Pemilihan2Percobaan1_26 {
         System.out.println("Masukkan Tahun : ");
         tahun = input26.nextInt();
         if ((tahun % 400) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) == 0)
             System.out.println("Tahun Kabisat");
         } else
          System.out.println("Bukan Tahun Kabisat");
