e94485e1cdec00461642b9e39811a832f8592524
Sariafauzani <ajas2654@gmail.com>
2023-10-05 08:20:09 +0700

    1

---
+++ Pemilihan2Percobaan1_26.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * Pemilihan2Percobaan1_26
+ */
+public class Pemilihan2Percobaan1_26 {
+    public static void main(String[] args) {
+        Scanner input26 = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("Masukkan Tahun : ");
+        tahun = input26.nextInt();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+        } else
+         System.out.println("Bukan Tahun Kabisat");
+    }
+}
