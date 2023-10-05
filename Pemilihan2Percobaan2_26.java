32b76128abadf0acaf804bdf92227726a774acc8
Sariafauzani <ajas2654@gmail.com>
2023-10-05 09:44:43 +0700

    modif 2

--- Pemilihan2Percobaan2_26.java
+++ Pemilihan2Percobaan2_26.java
@@ -13,11 +13,15 @@ public class Pemilihan2Percobaan2_26 {
     int totalSudut = sudut1 + sudut2 + sudut3;
 
     if (totalSudut == 180) {
-        if ((sudut1 == 90 || (sudut2 == 90) || (sudut3 == 90)))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-        else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-    } else
-    System.out.println("Bukan Segitiga");
-  }   
+        if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
+        System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+    else if ((sudut1 == sudut2 && sudut1 == sudut3))
+        System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+    else if ((sudut1 == sudut3) || (sudut2 == sudut1) || (sudut3 == sudut2))
+        System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+    else 
+        System.out.println("Segitiga tersebut adalah segitiga sembarang");
+    }else
+       System.out.println("Bukan Segitiga");
+  }
 }
